1983a7f432ad407f5f16af0575cc0ee505740625
Ares <pczapla@gazeta.pl>
2015-02-05 23:50:17 +0100

    before listing

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -10,6 +10,23 @@
 
 angular.module('storeApp')
   .controller('listCtrl', function ($scope) {
-  	
+
+  	// Parse.initialize("eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d", "LZDVFfrpgr7q5pJyFFE23NhziBxP6fcaieYNweWI");
+    var StoreInv = Parse.Object.extend("StoreInventory");
+    var storeInv = new StoreInv();
+
+
+	var query = new Parse.Query(StoreInv);
+
+	query.get("0QzK25ADnj", {
+		success: function(storeInv) {
+			console.log("it works!");
+		},
+		error: function(object,error) {
+
+		}
+	});
+
+    console.log(something);
 
   });
