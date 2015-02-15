6675be3533f273fd2ba6b128acd00d8587540536
Ares <pczapla@gazeta.pl>
2015-02-15 21:42:45 +0100

    'works'

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -9,34 +9,34 @@
  */
 
 angular.module('storeApp')
-  .controller('AddProdCtrl', function ($scope) {
+  .controller('AddProductCtrl', function ($scope) {
   	 	//$scope.product = '';
   	$scope.addProduct = function() {
   		// console.log($scope.product);
   		// console.log($scope.product.productName);
   		var products = { 
-  			"productName": $scope.product.productName,
-  			"netPrice" : $scope.product.netPrice,
-  			"invoiceNumber" : $scope.product.invoiceNumber,
-  			"invoiceDay" : $scope.product.invoiceDay,
-  			"measureUnit" : $scope.product.measureUnit,
-        "productVAT": $scope.product.productVAT
+  			'productName': $scope.product.productName,
+  			'netPrice' : $scope.product.netPrice,
+  			'invoiceNumber' : $scope.product.invoiceNumber,
+  			'invoiceDay' : $scope.product.invoiceDay,
+  			'measureUnit' : $scope.product.measureUnit,
+        'productVAT': $scope.product.productVAT
   			};	
 
-       // console.log(products);
+        // conole.log();
 
-  	Parse.initialize("eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d", "LZDVFfrpgr7q5pJyFFE23NhziBxP6fcaieYNweWI");
+  	// Parse.initialize('eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d', 'LZDVFfrpgr7q5pJyFFE23NhziBxP6fcaieYNweWI');
     
-    var StoreInventory = Parse.Object.extend("StoreInventory");
+    var StoreInventory = Parse.Object.extend('StoreInventory');
     var storeInventory = new StoreInventory();
       storeInventory.save(products, {
       success: function(object) {
-        $(".success").show();
-        $("input").val('');
+        $('.success').show();
+        $('input').val('');
         // console.log("it works!");
       },
       error: function(model, error) {
-        $(".error").show();
+        $('.error').show();
         // console.log("it's broken");
       }
     });
--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -11,22 +11,47 @@
 angular.module('storeApp')
   .controller('listCtrl', function ($scope) {
 
-  	// Parse.initialize("eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d", "LZDVFfrpgr7q5pJyFFE23NhziBxP6fcaieYNweWI");
-    var StoreInv = Parse.Object.extend("StoreInventory");
-    var storeInv = new StoreInv();
+  	$scope.hello = 'nice';
+
+  	Parse.initialize('eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d', 'LZDVFfrpgr7q5pJyFFE23NhziBxP6fcaieYNweWI');
+		var StoreInventory = Parse.Object.extend('StoreInventory');
+		var query = new Parse.Query(StoreInventory);
+
+		query.limit(1).find({
+			success: function(results) {
+				// alert("Success" +results.length);
+					var products = [];
+				for (var i=0; i<results.length;i++) {
+					var object = results[i];
+					var invoiceDay = object.attributes.invoiceDay;
+					// console.log(object);
+					// console.log(object.id);
+					// console.log(invoiceDay);
+					// console.log(object);					
+				}
+				return $scope.invoiceDay;
+			},
+			error: function(error) {
+				alert('Error' + error.code + ' ' + error.message);
+			}
+		})
 
 
-	var query = new Parse.Query(StoreInv);
 
-	query.get("0QzK25ADnj", {
-		success: function(storeInv) {
-			console.log("it works!");
-		},
-		error: function(object,error) {
+		// query.get("0QzK25ADnj", {
+		//   success: function(storeInventory) {
+		//     // The object was retrieved successfully.
+		//     console.log("success");
+		//     var invoiceDate = storeInventory.get("invoiceDay");
+		//     console.log(invoiceDate);
+		//   },
+		//   error: function(object, error) {
+		//     // The object was not retrieved successfully.
+		//     // error is a Parse.Error with an error code and message.
+		//     console.log("fail");
+		//   }
+		// });
 
-		}
-	});
 
-    console.log(something);
 
   });
