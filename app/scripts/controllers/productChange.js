b42e176ea0b41d0868a7e6da8f085b5dfdf7fa9c
Ares <pczapla@gazeta.pl>
2015-03-17 22:42:54 +0100

    initial product editing

---
+++ app/scripts/controllers/productChange.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the storeApp
+ */
+
+
+angular.module('storeApp')
+  .controller('productChangeCtrl', function ($scope, $routeParams, $http) {
+
+    $http.get('https://api.parse.com/1/classes/StoreInventory/'+$routeParams.productId,
+  			{
+  				headers:{
+                'X-Parse-Application-Id': 'eS1rNrAJQKbNEfm5AfA3jaY1Xajektnnu27XHT6d',
+                'X-Parse-REST-API-Key': '38FBR0WkiWMjMOzOt5gkU7EcXrTwvYHsNWnrx40k',
+                'Content-Type' : 'application/json'
+            },
+
+  		}).
+  		success( function(data,status) {
+  			// console.log('success');
+  			console.log(data+' '+status);
+  			// console.log(data.productName);
+
+  			var prod = data;
+
+  			$scope.prod = prod;
+
+
+  		}).
+  		error( function(data,status) {
+  			
+        $scope.message = data+' '+status;
+
+        // console.log('error');
+  			// console.log(data+' '+status);
+  		});
+
+  		// console.log($routeParams);
+
+ 		$scope.productId = $routeParams.productId;
+
+  		// var prod = $scope.prod;
+
+  	    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
